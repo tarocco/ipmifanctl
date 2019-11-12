f13f2aa9a13fa79413feca324a32bc899c2f8628
Tarocco <bun@taroc.co>
2019-11-12 02:49:20 -0800

    faux daemon, fixed subprocess call, explicit arguments

--- ipmifanctl1.py
+++ ipmifanctl1.py
@@ -1,19 +1,20 @@
 from argparse import ArgumentParser
 from ipmifanctl import *
+from time import sleep
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("temp_sensor_id")
-    parser.add_argument("lo_temp", type=int, default=30)
-    parser.add_argument("hi_temp", type=int, default=50)
-    parser.add_argument("lo_speed", type=int, default=5)
-    parser.add_argument("hi_speed", type=int, default=50)
-    parser.add_argument("power", type=float, default=2.0)
+
+    parser.add_argument('--temp-sensor-id')
+    parser.add_argument('--lo-temp', type=int, default=30)
+    parser.add_argument('--hi-temp', type=int, default=50)
+    parser.add_argument('--lo-speed', type=int, default=5)
+    parser.add_argument('--hi-speed', type=int, default=50)
+    parser.add_argument('--power', type=float, default=2.0)
+    parser.add_argument('--update-period', type=float, default=None)
     args = parser.parse_args()
 
-
-    print(dir(args))
     sensor = TemperatureSensor(args.temp_sensor_id)
 
     logic = Logic(
@@ -24,7 +25,12 @@ def main():
     fan_controller = RawFanControl()
     main_controller = Controller(sensor, logic, [fan_controller])
 
-    main_controller.update()
+    if args.update_period is None:
+        main_controller.update()
+    else:
+        while True:
+            main_controller.update()
+            sleep(args.update_period)
 
 
 if __name__ == '__main__':
